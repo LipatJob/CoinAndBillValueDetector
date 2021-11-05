d3071159331bb247483447393b1e8879d81dbd3e
jobert <EpicLegitPlayer@gmail.com>
2021-11-05 19:19:33 +0800

    improved video recog

--- video_recog.py
+++ video_recog.py
@@ -14,28 +14,26 @@ def main():
 
     print("CUDA enabled:", dlib.DLIB_USE_CUDA)
 
-    
+    frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     frame_size = (frame_width, frame_height)
-    fps = 3
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    writer = cv2.VideoWriter("videos/output.mp4", fourcc, fps, frame_size, True)
-    
-    
-    print("Cuda enabled:", dlib.DLIB_USE_CUDA) # true, if cuda is enabled.
+    writer = cv2.VideoWriter("videos/output.mp4", fourcc, frame_rate, frame_size, True)
 
     value_calculator = ValueCalculator()
 
+    current_frame = 0
+    
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
-            img = apply_preprocess(frame)
-            values = value_calculator.get_values(img)
+            if current_frame % frame_rate == 0:
+                img = apply_preprocess(frame)
+                values = value_calculator.get_values(img)
 
             display_values(values, frame)
 
-           
             if writer is not None:
                 writer.write(frame)
             
@@ -45,9 +43,12 @@ def main():
 
         else: 
             break
+
+        current_frame += 1
     
     cap.release()
     cv2.destroyAllWindows()
+    
 
 
 def get_value_from_image(current_frame):
